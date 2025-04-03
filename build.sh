e461c3d9b77e820d8ea5cebd931ca186baa251b6
TanNhatCMS <113796420+TanNhatCMS@users.noreply.github.com>
2025-04-03 18:58:17 +0700

    chore fix RAM

---
+++ build.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo "🚀 Đang khởi động container docker..."
+docker-compose up -d --build
--- ‎init.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
- echo "🚀 Đang khởi động container docker..."
- docker-compose up -d --build
